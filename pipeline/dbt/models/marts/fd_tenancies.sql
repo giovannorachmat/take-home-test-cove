207722bc69e36352825c394899e535db1f19eef4
Giovanno Rachmat <giovannorachmat@Giovannos-MacBook-Pro.local>
2026-07-21 10:53:04 +0700

    updated dbt models

--- pipeline/dbt/models/marts/fd_tenancies.sql
+++ pipeline/dbt/models/marts/fd_tenancies.sql
@@ -1,73 +1,72 @@
-with rooms as (
-    select
-        id as room_id
-        , updated_at as room_updated_at
-        , * except (id, updated_at, is_deleted)
-    from
-        {{ ref('stg_rooms') }}
-)
+-- noqa: disable=LT01,LT05,LT02
 
-, properties as (
-    select
-        id as property_id
-        , updated_at as property_updated_at
-        , property_name
-        , property_city
-        , * except (id,
-        property_name,
-        property_city,
-        updated_at,
-        is_deleted)
-    from
-        {{ ref('stg_properties') }}
-)
-
-, rooms_properties as (
-    select
-        room_id
-        , room_updated_at
-        , room_number
-        , room_type
-        , property_id
-        , property_name
-        , property_city
-        , property_updated_at
-        , lease_start_date
-        , lease_end_date
-    from
-        rooms
-    left join
-        properties
-        using (property_id)
-)
-
-, tenancies as (
-    select
-        *
-    from
-        {{ ref('stg_tenancies') }}
-)
-
-, parsed_all as (
-    select
-        *
-    from
-        tenancies
-    left join
-        rooms_properties
-        using (room_id)
-)
-
-select
-    id
-    , tenant_id
-    , check_in_date
-    , check_out_date
-    , status
-    , updated_at
-    , room_id
-    , room_updated_at
-    , coalesce(property_name, 'UNKNOWN') as property_name
-    , coalesce(property_city, 'UNKNOWN') as property_city
-from parsed_all
-order by updated_at, id
+WITH rooms AS (
+    SELECT
+      id AS room_id,
+      updated_at AS room_updated_at,
+      * EXCEPT (id, updated_at, is_deleted)
+    FROM
+      {{ ref('stg_rooms') }}
+  ),
+  properties AS (
+    SELECT
+      id AS property_id,
+      updated_at AS property_updated_at,
+      property_name,
+      property_city,
+      *
+        EXCEPT (
+          id,
+          property_name,
+          property_city,
+          updated_at,
+          is_deleted)
+    FROM
+      {{ ref('stg_properties') }}
+  ),
+  rooms_properties AS (
+    SELECT
+      room_id,
+      room_updated_at,
+      room_number,
+      room_type,
+      property_id,
+      property_name,
+      property_city,
+      property_updated_at,
+      lease_start_date,
+      lease_end_date
+    FROM
+      rooms
+    LEFT JOIN
+      properties
+      USING (property_id)
+  ),
+  tenancies AS (
+    SELECT
+      *
+    FROM
+      {{ ref('stg_tenancies') }}
+  ),
+  parsed_all AS (
+    SELECT
+      *
+    FROM
+      tenancies
+    LEFT JOIN
+      rooms_properties
+      USING (room_id)
+  )
+SELECT
+  id,
+  tenant_id,
+  check_in_date,
+  check_out_date,
+  status,
+  updated_at,
+  room_id,
+  room_updated_at,
+  coalesce(property_name, 'UNKNOWN') AS property_name,
+  coalesce(property_city, 'UNKNOWN') AS property_city
+FROM parsed_all
+ORDER BY updated_at, id
--- pipeline/dbt/models/marts/fm_occupancy_rate.sql
+++ pipeline/dbt/models/marts/fm_occupancy_rate.sql
@@ -1,78 +1,70 @@
-with
-active_rooms as (
-    select
-        r.id as room_id
-        , r.room_number
-        , r.room_type
-        , r.property_id
-        , p.property_name
-        , p.property_city
-        , r.deleted_at as room_deleted_at
-        , p.deleted_at as property_deleted_at
-        , p.lease_start_date
-        , p.lease_end_date
-    from
-        {{ ref('stg_rooms') }} r
-    inner join
-        {{ ref('stg_properties') }} p
-        on r.property_id = p.id
-)
+-- noqa: disable=LT01,LT05
 
-, daily_availability as (
-    select
+WITH active_rooms AS (
+    SELECT
+        r.id AS room_id,
+        r.room_number,
+        r.room_type,
+        r.property_id,
+        p.property_name,
+        p.property_city,
+        r.deleted_at AS room_deleted_at,
+        p.deleted_at AS property_deleted_at,
+        p.lease_start_date,
+        p.lease_end_date
+    FROM
+    {{ ref('stg_rooms') }} r
+    LEFT JOIN
+    {{ ref('stg_properties') }} p
+    ON r.property_id = p.id
+),
+
+daily_availability AS (
+    SELECT
         d.date
         , ar.room_id
         , ar.property_id
         , ar.property_name
-    from
-        {{ ref('dim_date') }} d
-    left join
-        active_rooms ar
-        on
-            d.date between ar.lease_start_date and ar.lease_end_date
-            and (ar.room_deleted_at is null or d.date < ar.room_deleted_at)
-            and (
-                ar.property_deleted_at is null
-                or d.date < ar.property_deleted_at
-            )
-    where
-        ar.room_id is not null
+    FROM
+    {{ ref('dim_date') }} d
+    , active_rooms ar
+    WHERE
+    TRUE
+    AND ar.room_id IS NOT NULL
+    AND d.date BETWEEN ar.lease_start_date AND ar.lease_end_date
+    AND (ar.room_deleted_at IS NULL OR d.date < ar.room_deleted_at)
+    AND (ar.property_deleted_at IS NULL OR d.date < ar.property_deleted_at)
+),
+
+daily_occupancy AS (
+    SELECT DISTINCT
+        d.date, t.room_id
+    FROM
+    {{ ref('dim_date') }} d
+    , {{ ref('stg_tenancies') }} t
+    WHERE
+    TRUE
+    AND t.room_id IS NOT NULL
+    AND d.date BETWEEN t.check_in_date AND date_sub (t.check_out_date, INTERVAL 1 day)
+    AND t.status != 'CANCELLED'
 )
 
-, daily_occupancy as (
-    select distinct
-        d.date
-        , t.room_id
-    from
-        {{ ref('dim_date') }} d
-    left join
-        {{ ref('stg_tenancies') }} t
-        on
-            d.date between t.check_in_date
-            and date_sub(t.check_out_date, interval 1 day)
-            and t.status != 'CANCELLED'
-    where
-        t.room_id is not null
-)
-
-select
-    da.property_name
-    , date_trunc(da.date, month) as month
-    , count(*) as available_room_days
-    , count(occ.room_id) as occupied_room_days
-    , round(count(occ.room_id) / count(*), 4) as occupancy_rate
-from
+SELECT
+    da.property_name,
+    date_trunc(da.date, month) AS month,
+    count(*) AS available_room_days,
+    count(occ.room_id) AS occupied_room_days,
+    round(count(occ.room_id) / count(*), 4) AS occupancy_rate
+FROM
     daily_availability da
-left join
+LEFT JOIN
     daily_occupancy occ
-    on
+    ON
         da.date = occ.date
-        and da.room_id = occ.room_id
-group by
-    da.property_name
-    , date_trunc(da.date, month)
-having
+        AND da.room_id = occ.room_id
+GROUP BY
+    da.property_name, date_trunc(da.date, month)
+HAVING
     count(*) > 0
-order by
-    da.property_name
-    , month
+ORDER BY
+    da.property_name, month
